9f8c638a8395ad1e9960dcc180cf8f54028fb34c
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-12 21:49:31 +0100

    edit footer

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ export default function App() {
       <div className="container">
       <Weather/>
       <footer>
-        This project was coded by {""}<a href="https://www.linkedin.com/in/oleksandra-murashko-1b14a4254/" target="_blank">Oleksandra Murashko</a> {""} and is{" "}
+        {/* This project was coded by {""}<a href="https://www.linkedin.com/in/oleksandra-murashko-1b14a4254/" target="_blank">Oleksandra Murashko</a> {""} and is{" "}
       <a href="https://github.com/Aleksandra2503/weather-r-5"
       target="_blank">
         open-sourced on GitHub
       </a>
+       */}
+
+    This project was coded by Murashko Oleksandra and is{" "}
+            <a
+              href="https://github.com/Aleksandra2503/weather-r-5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Oleksandra Murashko on linkedin
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://www.linkedin.com/in/oleksandra-murashko-1b14a4254/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
       </footer>
       </div>
     </div>
