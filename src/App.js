41f2d827680e97ad695d42319ca13641d37d137d
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-12 23:55:35 +0100

    Api integration

--- src/App.js
+++ src/App.js
@@ -6,31 +6,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather/>
+      <Weather defaulrCity="Murnau" />
       <footer>
-        {/* This project was coded by {""}<a href="https://www.linkedin.com/in/oleksandra-murashko-1b14a4254/" target="_blank">Oleksandra Murashko</a> {""} and is{" "}
+        This project was coded by {""}<a href="https://www.linkedin.com/in/oleksandra-murashko-1b14a4254/" target="_blank" rel="noopener noreferrer">Oleksandra Murashko</a> {""} and is{" "}
       <a href="https://github.com/Aleksandra2503/weather-r-5"
-      target="_blank">
+      target="_blank" rel="noopener noreferrer">
         open-sourced on GitHub
       </a>
-       */}
-
-    This project was coded by Murashko Oleksandra {" "}
-            <a
-              href="https://github.com/Aleksandra2503/weather-r-5"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              open-sourced on GitHub
-            </a>{" "}
-            . I am in{" "}
-            <a
-              href="https://www.linkedin.com/in/oleksandra-murashko-1b14a4254/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-               linkedin.
-            </a>
       </footer>
       </div>
     </div>
