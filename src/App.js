ff937a88b9d8cb5dc65cb33b9cd5d4c748a672cc
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-12 18:41:17 +0100

    btn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+      <Weather/>
       <footer>
         This project was coded by {""}<a href="https://www.linkedin.com/in/oleksandra-murashko-1b14a4254/" target="_blank">Oleksandra Murashko</a> {""} and is{" "}
       <a href="https://github.com/Aleksandra2503/weather-r-5"
