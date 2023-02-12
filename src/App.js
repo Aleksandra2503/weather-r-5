a3a01ffdd58eb30d6dffa520b3e74f1fc7690916
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-12 16:49:22 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
-        This project was coded by Oleksandra Murashko and is 
+        This project was coded by {""}<a href="https://www.linkedin.com/in/oleksandra-murashko-1b14a4254/" target="_blank">Oleksandra Murashko</a> {""} and is{" "}
       <a href="https://github.com/Aleksandra2503/weather-r-5"
       target="_blank">
         open-sourced on GitHub
