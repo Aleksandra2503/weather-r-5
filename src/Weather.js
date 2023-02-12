8d3e52ae8b16f59add02e40255920a96bc83a7a9
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-12 20:02:57 +0100

    added img

--- src/Weather.js
+++ src/Weather.js
@@ -3,15 +3,15 @@ import './Weather.css';
 
 export default function Weather() {
 return(
-    <div className='Weather'>
+    <div className="Weather">
         <form>
             <div className="row">
                 <div className="col-9">
             <input type="search" placeholder="Enter a city.."
-            className="form-control"/>
+            className="form-control" autofocus="on"/>
                 </div>
                 <div className="col-3">
-                    <input type="submit" value="Search" className="btn btn-primary" />
+                    <input type="submit" value="Search" className="btn btn-primary w-100" />
                 </div>
             </div>
         </form>
@@ -20,10 +20,16 @@ return(
          <li>Wednesday 7:00</li>
          <li>Mostly Cloudy</li>
        </ul>
-       <div className="row">
+       <div className="row mt-3">
         <div className="col-6">
-          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy"/>
-          6°C
+            <div className="clearfix">
+          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy" className="float-left"
+          />
+         <div className="float-left">
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
+         </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
