c530929985766308754086502c83f0cdf713fc4f
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-12 17:06:44 +0100

    applicated

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by {""}<a href="https://www.linkedin.com/in/oleksandra-murashko-1b14a4254/" target="_blank" rel="noreferrer">Oleksandra Murashko</a> {""} and is{" "}
+        This project was coded by {""}<a href="https://www.linkedin.com/in/oleksandra-murashko-1b14a4254/" target="_blank">Oleksandra Murashko</a> {""} and is{" "}
       <a href="https://github.com/Aleksandra2503/weather-r-5"
-      target="_blank" rel="noreferrer">
+      target="_blank">
         open-sourced on GitHub
       </a>
       </footer>
