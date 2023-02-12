26f066322ee029c29b4b19c296dc5c3034c3cbff
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-12 16:37:36 +0100

    change 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by Oleksandra Murashko and is 
+      <a href="https://github.com/Aleksandra2503/weather-r-5"
+      target="_blank">
+        open-sourced on GitHub
+      </a>
+      </footer>
     </div>
   );
 }
