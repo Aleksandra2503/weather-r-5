8d7b74cc6fa5810932fb6de1e1a8e60497d05ddc
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-13 22:27:58 +0100

    11

--- src/Weather.js
+++ src/Weather.js
@@ -31,31 +31,38 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "6f2c8d8e771e0ede0a15520e397b527c";
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
-    if (weatherData.ready) {
-      return (
-        <div className="Weather">
-            <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col-9">
-                <input type="search" placeholder="Enter a city.."
-                className="form-control" autoFocus="on" onChange={handleCityChange}
-                />
-                    </div>
-                    <div className="col-3">
-                        <input type="submit" value="Search" className="btn btn-primary w-100" />
-                    </div>
-                </div>
-                </form>
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
         <WeatherInfo data={weatherData} />
         <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
-
   } else {
     search();
     return "Loading...";
